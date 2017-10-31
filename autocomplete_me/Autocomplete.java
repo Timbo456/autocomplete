6c90be1f3ca3b1d13d5014bd40e6ed02e0759726
Sam <samtaylor720@gmail.com>
2017-10-30 21:34:19 -0400

    Revert "Revert "Shannon""
    This reverts commit d1dfee0221f595c3eaf4334d9990467ed70ce8ce.

---
+++ autocomplete_me/Autocomplete.java
@@ -0,0 +1,50 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import java.util.Arrays;
+import java.util.Comparator;
+
+// A data type that provides autocomplete functionality for a given set of 
+// string and weights, using Term and BinarySearchDeluxe. 
+public class Autocomplete {
+    ...
+    
+    // Initialize the data structure from the given array of terms.
+    public Autocomplete(Term[] terms) {
+        ...
+    }
+
+    // All terms that start with the given prefix, in descending order of
+    // weight.
+    public Term[] allMatches(String prefix) {
+        ...
+    }
+
+    // The number of terms that start with the given prefix.
+    public int numberOfMatches(String prefix) {
+        ...
+    }
+
+    // Entry point. [DO NOT EDIT]
+    public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Term[] terms = new Term[N];
+        for (int i = 0; i < N; i++) {
+            long weight = in.readLong(); 
+            in.readChar(); 
+            String query = in.readLine(); 
+            terms[i] = new Term(query.trim(), weight); 
+        }
+        int k = Integer.parseInt(args[1]);
+        Autocomplete autocomplete = new Autocomplete(terms);
+        while (StdIn.hasNextLine()) {
+            String prefix = StdIn.readLine();
+            Term[] results = autocomplete.allMatches(prefix);
+            for (int i = 0; i < Math.min(k, results.length); i++) {
+                StdOut.println(results[i]);
+            }
+        }
+    }
+}
